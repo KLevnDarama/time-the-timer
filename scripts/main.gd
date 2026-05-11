7bf0d8d6838da963f7e770f325c5f9af4c11c490
Syauqi Ramdhani <notpitianqi42@gmail.com>
2026-05-12 01:20:46 +0700

    feat: add sound effect for hint
    Added 'Ding' sound effect everytime it hits modulo 1000 ms at a range.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,9 +3,14 @@ extends ColorRect
 @onready var main_button: Button = $MainButton
 @onready var guide: Label = $Guide
 @onready var hint: Label = $Hint
+@onready var ding_sound: AudioStreamPlayer = $"../DingSound"
+
+@export var hintTime = 3500
 
 var timeSession = 0
 var timeTarget = 0
+var dingDebounce = false
+
 enum SessionState { IDLE, TIMING, RESULT }
 var currentSession = SessionState.IDLE
 
@@ -33,15 +38,19 @@ func set_session(state: SessionState) -> SessionState:
 		_:
 			return SessionState.IDLE
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	currentSession = set_session(SessionState.IDLE)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if currentSession == SessionState.TIMING:
 		var deltaHint = Time.get_ticks_msec() - timeSession
-		hint.text = "%.2f" % (deltaHint/1000.0) if deltaHint <= 3000 else ""
+		hint.text = "%.2f" % (deltaHint/1000.0) if deltaHint <= hintTime else ""
+		if deltaHint <= hintTime and deltaHint % 1000 < 100 and not dingDebounce:
+			dingDebounce = true
+			ding_sound.play()
+		
+		if deltaHint % 1000 > 100:
+			dingDebounce = false
 
 func _on_main_button_down() -> void:
 	match currentSession:
